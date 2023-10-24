ebe1a0d745b7066ad16e714edf31e62ca81a218b
adnanahmad123786 <148853992+adnanahmad123786@users.noreply.github.com>
2023-10-24 13:28:06 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,70 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import ApiComponent from "./component/ApiComponent";
 
 export default function App() {
+
+  const [data070, setData070] = useState(null);
+  async function getData(){
+    const res = await AsyncStorage.getItem("data");
+    if (res !== null){
+      console.log("Response in app")
+      setData070(JSON.parse(res));
+
+    } 
+  }
+  useEffect(()=>{
+    
+
+    getData();
+  },[])
+
+  const renderItem = ({ item }) => (
+    <View>
+        <Text> Bitcoin exchange rate for {item.symbol} </Text>
+      
+      <Text > {item.rate} </Text>
+    </View>
+  );
+
+
   return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<ApiComponent />
+			
+			<ApiComponent/>
+
+
+        
+
+      <View style={{flex: 1, paddingTop: '40%'}}>
+      <FlatList
+        data={data070}
+        renderItem={renderItem}
+      />
+      </View>
+ 
+    
+      
+
 			<StatusBar style="auto" />
+
+      
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
+  
+
 });
