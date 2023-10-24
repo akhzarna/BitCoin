ac840a2586155132cbe0ebe6f6958f37cf0b46ff
AazhmeerChhapra <aazhmeerchhapra@gmail.com>
2023-10-24 12:49:37 +0500

    aazhmeerChhapra039

--- App.js
+++ App.js
@@ -1,16 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import ApiComponent from "./component/ApiComponent";
 
-export default function App() {
+
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CustomHooks from './CustomHooks';
+
+
+const App = () => {
+  const [res, setResponseData] = useState({});
+  const [currData, setCurrencyData] = useState([]);
+  const {responseData, currencyData, GetAPIHooks} = CustomHooks();
+
+
+  useEffect(() => {
+
+    GetAPIHooks('https://api.coindesk.com/v1/bpi/currentprice.json');
+
+    }, []);
+  
+
+  // const renderItem = ({ item }) => (
+    
+  // );
+
   return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<ApiComponent />
-			<StatusBar style="auto" />
-		</View>
-	);
-}
+    <View style={styles.container}>
+      <FlatList
+        data={currencyData}
+        renderItem={({item}) =>
+         
+          <View style={styles.card}>
+      <Text style={styles.currency}>{item.currency}</Text>
+      <Text style={styles.rate}>{item.rate}</Text>
+    </View>
+        }
+        keyExtractor={(item) => item.currency}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,5 +47,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:40
+  },
+  card: {
+    backgroundColor: '#fff',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  currency: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  rate: {
+    fontSize: 16,
+    color: 'green',
+  },
+  symbol: {
+    fontSize: 16,
   },
 });
+
+export default App;
