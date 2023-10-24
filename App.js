d1ae8746cf6ea7fbff029cfd0be9ee4d8b2b4433
Mehr-zain <72857599+Mehr-zain@users.noreply.github.com>
2023-10-24 13:18:21 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ApiComponent from "./component/ApiComponent";
 export default function App() {
   return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<Text>Hello Sir This is Zain From FA20.My Roll No is FA20-BSE-088</Text>
 			<ApiComponent />
 			<StatusBar style="auto" />
 		</View>
