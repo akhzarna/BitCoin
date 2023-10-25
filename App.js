e46924ab66652bf1c1930f9acd11144836d74819
Hafiz Muhammad Abdullah <72753811+hafizabdullah510@users.noreply.github.com>
2023-10-26 00:10:55 +0500

    first commit

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 import ApiComponent from "./component/ApiComponent";
 
 export default function App() {
   return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<ApiComponent />
-			<StatusBar style="auto" />
-		</View>
-	);
+    <View style={styles.container}>
+      <Text>Open up to start working on your app!</Text>
+      <ApiComponent />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
